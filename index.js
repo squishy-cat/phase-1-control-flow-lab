e24ecc803e0de9a28b3cb036b0da1b7b63abcfb6
Eve Kozina <eve.kozina@gmail.com>
2021-11-01 12:10:29 -0400

    commit tree 861fe0c5846878854dbbe677103ba8da74cbc4d0

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+    let messageDistance;
+    if (distance <= 400) {
+      messageDistance = 'This one is on me!';
+    }else if (distance > 2000, distance < 2500) {
+      messageDistance = 'I will gladly take your thirty bucks.';
+    }else if (distance > 2500) {
+      messageDistance = 'No can do.';
+    } 
+    return messageDistance;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const messageDestination = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return messageDestination;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  let tipMessage;
+  switch (tipAmount) {
+    case 'generous':
+      tipMessage = "Thank you so much.";
+      break;
+    case 'not as generous':
+      tipMessage = "Thank you.";
+      break;
+    default:
+      tipMessage = "Bye.";
+      break;
+  }
+  return tipMessage;
 }
